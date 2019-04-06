c500b7442410e6ae6e0e413ce44687cfa1cb6e71
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-04-06 19:32:34 +0300

    Task API tests

--- src/test/java/com/sdehunt/TaskApiTest.java
+++ src/test/java/com/sdehunt/TaskApiTest.java
@@ -1,19 +1,68 @@
 package com.sdehunt;
 
+import com.sdehunt.model.Task;
+import com.sdehunt.model.impl.TaskImpl;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.is;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class TaskApiTest {
 
+    private final static String BASE_URI = "http://localhost";
+    private final static int PORT = 8080;
+    private final static String APP_JSON = "application/json";
+
+    private final static String TASKS_PATH = "/tasks/";
+    private final static int SUCCESS = 200;
+
+
     @Test
     public void getAllTasks() {
-        // TODO verify size is 2
-        get("/tasks")
+
+        Task task = new TaskImpl("task-description-" + UUID.randomUUID());
+
+        // Saving task
+        String id = host()
+                .body(task)
+                .contentType(APP_JSON)
+                .post(TASKS_PATH)
+                .asString();
+
+        // Verify created
+        host().contentType(APP_JSON)
+                .get(TASKS_PATH + id)
                 .then()
-                .body("size()", is(2));
+                .statusCode(SUCCESS)
+                .body("description", equalTo(task.getDescription()));
+
+        // Getting all tasks
+        host().log().all()
+                .get(TASKS_PATH)
+                .then()
+                .log().all()
+                .statusCode(SUCCESS)
+                .body("size()", is(1));
+
+        // Deleting task
+        host().delete(TASKS_PATH + id)
+                .then()
+                .statusCode(SUCCESS);
+
+        // Verify deleted
+        host().get(TASKS_PATH)
+                .then()
+                .statusCode(SUCCESS)
+                .body("size()", is(0));
+    }
+
+    private RequestSpecification host() {
+        return given().baseUri(BASE_URI).port(PORT);
     }
 
 }
