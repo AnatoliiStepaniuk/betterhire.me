f689c60be365e9c2e8a39c171512d3777e797bb8
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-04-14 16:23:17 +0300

    Verify script (waiting for app to start)

---
+++ scripts/verify.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Build
+mvn clean install -DskipITs && docker build -f api/Dockerfile . -t sdehunt
+
+# Wait for application to start
+containerId=$(docker run -d -v ~/.aws:/.aws -p 80:8080 sdehunt)
+until grep -q 'Started Application' <<< $(docker logs $containerId); do sleep 2; done
+
+# Tests
+mvn verify
+
+# Clean up
+docker stop $(docker ps -q)
