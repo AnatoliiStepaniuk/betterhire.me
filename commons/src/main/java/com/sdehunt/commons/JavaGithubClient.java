0331c06b442e6e81bc9051159f50397c45cf9fbf
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-05-26 11:26:30 +0300

    Logger

--- commons/src/main/java/com/sdehunt/commons/JavaGithubClient.java
+++ commons/src/main/java/com/sdehunt/commons/JavaGithubClient.java
@@ -7,6 +7,8 @@ import com.sdehunt.commons.params.ParameterService;
 import com.sdehunt.commons.params.SsmParameterService;
 import lombok.Data;
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -34,28 +36,28 @@ public class JavaGithubClient implements GithubClient {
     private final ParameterService params;
     private final HttpClient client;
     private final ObjectMapper objectMapper;
-
+    private final Logger logger;
 
     public JavaGithubClient() {
         this.client = HttpClient.newHttpClient();
         this.objectMapper = new ObjectMapper()
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         this.params = new HardCachedParameterService(new SsmParameterService());
+        this.logger = LoggerFactory.getLogger(JavaGithubClient.class);
     }
 
     @SneakyThrows
     public void download(String repo, String commit, String file) {
         URI uri = new URI(RAW_DOMAIN + "/" + repo + "/" + commit + "/" + file);
 
-        // TODO replace with logger
-        System.out.println(String.format("Downloading file %s of repo %s for commit %s", file, repo, commit));
+        logger.debug(String.format("Downloading file %s of repo %s for commit %s", file, repo, commit));
         HttpResponse<Path> response = client.send(buildRequest(uri), HttpResponse.BodyHandlers.ofFile(createFile(file)));
-        System.out.println(String.format("Downloaded file %s of repo %s for commit %s", file, repo, commit));
+        logger.debug(String.format("Downloaded file %s of repo %s for commit %s", file, repo, commit));
 
         if (response.statusCode() == 503) { // Rarely reproduced issue of returning 503 status code
-            System.out.println(String.format("Second attempt to download file %s of repo %s for commit %s", file, repo, commit));
+            logger.warn(String.format("Second attempt to download file %s of repo %s for commit %s", file, repo, commit));
             response = client.send(buildRequest(uri), HttpResponse.BodyHandlers.ofFile(createFile(file)));
-            System.out.println(String.format("Download file %s of repo %s for commit %s from second attempt", file, repo, commit));
+            logger.warn(String.format("Download file %s of repo %s for commit %s from second attempt", file, repo, commit));
         }
 
         if (response.statusCode() != 200) {
