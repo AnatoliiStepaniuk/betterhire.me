37ac29afd6f9da9755b2aa612f4b29f129eb1b65
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-06-02 21:56:45 +0300

    compose restart update

--- scripts/compose_restart.sh
+++ scripts/compose_restart.sh
@@ -3,4 +3,5 @@ echo REACT_APP_SERVER_URL=$(curl http://169.254.169.254/latest/meta-data/public-
 docker login
 docker pull sdehuntdeveloper/sdehunt:latest
 docker pull sdehuntdeveloper/sdehunt_client:latest
-docker-compose restart
+docker-compose down
+docker-compose up -d
