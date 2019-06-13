6e33f3fa375c7ac534b733fc4a4e7ba20ee98676
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-06-13 12:28:16 +0300

    changed port 3001 to 80

--- scripts/compose_restart.sh
+++ scripts/compose_restart.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo REACT_APP_SERVER_URL=$(curl http://169.254.169.254/latest/meta-data/public-hostname):8080 > frontend.env
-echo FRONT_END_HOST=$(curl http://169.254.169.254/latest/meta-data/public-hostname) > backend.env
-echo FRONT_END_PORT=3001 >> backend.env
 docker login
 docker pull sdehuntdeveloper/sdehunt:latest
 docker pull sdehuntdeveloper/sdehunt_client:latest
