7f3a8ca626f5278a6fb49e62c2e4355463203d03
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-06-13 22:36:44 +0300

    unhardcode of clientId and clientSecret

--- scripts/scp.sh
+++ scripts/scp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-scp -i "last-key-pair.pem" scripts/pull_and_run.sh ec2-user@$1:/home/ec2-user/pull_and_run.sh
-scp -i "last-key-pair.pem" scripts/run.sh ec2-user@$1:/home/ec2-user/run.sh
-scp -i "last-key-pair.pem" scripts/clean.sh ec2-user@$1:/home/ec2-user/clean.sh
-scp -i "last-key-pair.pem" scripts/compose_restart.sh ec2-user@$1:/home/ec2-user/compose_restart.sh
-scp -i "last-key-pair.pem" docker-compose.yml ec2-user@$1:/home/ec2-user/docker-compose.yml
+host=52.28.237.192
+scp -i "last-key-pair.pem" scripts/pull_and_run.sh ec2-user@$host:/home/ec2-user/pull_and_run.sh
+scp -i "last-key-pair.pem" scripts/run.sh ec2-user@$host:/home/ec2-user/run.sh
+scp -i "last-key-pair.pem" scripts/clean.sh ec2-user@$host:/home/ec2-user/clean.sh
+scp -i "last-key-pair.pem" scripts/compose_restart.sh ec2-user@$host:/home/ec2-user/compose_restart.sh
+scp -i "last-key-pair.pem" docker-compose.yml ec2-user@$host:/home/ec2-user/docker-compose.yml
 # .aws should not be copied. use # aws config on the instance
