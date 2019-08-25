2b9106668b92af572859c2a940a858960ab08d57
AnatoliiStepaniuk <anatolii.stepaniuk@gmail.com>
2019-08-25 10:05:34 +0300

    migration fix

--- migrations/migration8.sql
+++ migrations/migration8.sql
@@ -16,7 +16,29 @@ ALTER TABLE `template`
 INSERT INTO `task` (`task`, `description`, `created`, `name`, `short_description`, `enabled`, `submittable`,
                     `image_url`, `test`, `tags`, `languages`, `job`, `job_url`, `company`, `requirements`)
 VALUES ('city',
-        '# Cities   Combine cities so that the next one starts with the last letter of the previous, like:    Pari**s**->**S**tockhol**m**->**M**ilan           Your score is the **number of letters** of all cities in your list.      ### Example:   Let\'s say `input.txt` has these cities:   ```   London   Milan   Stockholm   Paris   ```   Your `output.txt` file can look like this::   ```   Paris   Stockholm   Milan   ```   Your score for this solution is 19 (total number of letters in all cities).      #### All necessary files are present in your solution repository. Click the button and start coding!',
+        '# Cities
+Combine cities so that the next one starts with the last letter of the previous, like:
+ Pari**s**->**S**tockhol**m**->**M**ilan
+
+   Your score is the **number of letters** of all cities in your list.
+
+### Example:
+Let''s say `input.txt` has these cities:
+```
+London
+Milan
+Stockholm
+Paris
+```
+Your `output.txt` file can look like this::
+```
+Paris
+Stockholm
+Milan
+```
+Your score for this solution is 19 (total number of letters in all cities).
+
+#### All necessary files are present in your solution repository. Click the button and start coding!',
         '1566671043',
         'Junior Java Developer',
         'Combine cities so that the next one starts with the last letter of the previous',
@@ -37,7 +59,44 @@ VALUES ('city', 'java', 'city_java');
 INSERT INTO `task` (`task`, `description`, `created`, `name`, `short_description`, `enabled`, `submittable`,
                     `image_url`, `test`, `tags`, `languages`, `job`, `job_url`, `company`, `requirements`)
 VALUES ('letter',
-        '# Letters Game      You need to create words from a set of random letters. Each letter can be used at most once. Each word can be used at most once. Your total score is a number of letters in all words you''ve created.      ## Input files format      Files come in pairs - with words and with letters.  Named respectivly `a_letters.txt`, `a_words.txt`, `b_letters.txt`, `b_words.txt`, etc.      ## Output file format   Letters files should be named `a_result.txt`, `b_result.txt`, etc. and contain words separated by newline.      ### Example:   File `a_words.txt` has words:   ```   better   hire   me   ```   File `a_letters.txt` has letters:   ```   i   h   m   b   r   e   t   r   e   ```   From these letters you can create 2 words (`hire` and `me`), so the file should have the following content:   ```   hire   me   ```   Your score for this solution is 6 (total number of letters in all words).      ### [Fork repository template](https://github.com/BetterHireMe/letters_solution) and start coding!',
+        '# Letters Game
+
+You need to create words from a set of random letters. Each letter can be used at most once. Each word can be used at most once. Your total score is a number of letters in all words you''ve created.
+
+## Input files format
+
+Files come in pairs - with words and with letters.  Named respectivly `a_letters.txt`, `a_words.txt`, `b_letters.txt`, `b_words.txt`, etc.
+
+## Output file format
+Letters files should be named `a_result.txt`, `b_result.txt`, etc. and contain words separated by newline.
+
+### Example:
+File `a_words.txt` has words:
+```
+better
+hire
+me
+```
+File `a_letters.txt` has letters:
+```
+i
+h
+m
+b
+r
+e
+t
+r
+e
+```
+From these letters you can create 2 words (`hire` and `me`), so the file should have the following content:
+```
+hire
+me
+```
+Your score for this solution is 6 (total number of letters in all words).
+
+#### All necessary files are present in your solution repository. Click the button and start coding!',
         '1566711495',
         'Junior JavaScript Developer',
         'Arrange letters into words. Pretty simple, huh?',
