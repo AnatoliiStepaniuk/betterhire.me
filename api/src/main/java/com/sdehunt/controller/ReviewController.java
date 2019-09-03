b6ee0c51eca434ec4eccbed632c8ae8a15edf33d
AnatoliiStepaniuk <anatolii.stepaniuk@gmail.com>
2019-09-03 23:04:58 +0300

    shortcut route for reviews

--- api/src/main/java/com/sdehunt/controller/ReviewController.java
+++ api/src/main/java/com/sdehunt/controller/ReviewController.java
@@ -2,9 +2,12 @@ package com.sdehunt.controller;
 
 import com.sdehunt.commons.model.Review;
 import com.sdehunt.commons.model.Solution;
+import com.sdehunt.commons.model.User;
 import com.sdehunt.dto.SaveReviewDTO;
 import com.sdehunt.repository.ReviewRepository;
 import com.sdehunt.repository.SolutionRepository;
+import com.sdehunt.repository.UserRepository;
+import com.sdehunt.repository.impl.SolutionQueryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +23,9 @@ public class ReviewController {
     @Autowired
     private SolutionRepository solutions;
 
+    @Autowired
+    private UserRepository users;
+
     @RequestMapping(
             method = RequestMethod.POST, path = "/solutions/{solutionId}/review",
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -32,6 +38,21 @@ public class ReviewController {
     ) {
         Solution solution = solutions.get(solutionId).orElseThrow();
         reviews.create(solutionId, solution.getUserId(), solution.getTaskId(), req.getGrade(), req.getComment(), req.getEmoji(), "betterhire");
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST, path = "/user/github/{github}/review/last",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void reviewLastSolutionByGithubLogin(
+            @PathVariable String github,
+//            @CurrentUser UserPrincipal currentUser, // TODO use it as reviewer
+            @RequestBody SaveReviewDTO req
+    ) {
+        User user = users.byGithubLogin(github).orElseThrow();
+        Solution solution = solutions.query(new SolutionQueryImpl().user(user.getId())).get(0);
+        reviews.create(solution.getId(), solution.getUserId(), solution.getTaskId(), req.getGrade(), req.getComment(), req.getEmoji(), "betterhire");
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/reviews/user/{userId}")
