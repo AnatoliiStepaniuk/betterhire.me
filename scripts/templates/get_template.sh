e7a1422f89403f9ddda86f13c417c72c51a9a89b
AnatoliiStepaniuk <anatolii.stepaniuk@gmail.com>
2019-09-09 21:17:40 +0300

    Scripts for email template management

---
+++ scripts/templates/get_template.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+aws ses get-template --template-name MyTemplate --region eu-west-1
---
+++ scripts/templates/update_template.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+aws ses update-template --cli-input-json file://scripts/templates/template.json --region eu-west-1
